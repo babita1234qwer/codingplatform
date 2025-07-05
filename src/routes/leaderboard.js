5443ad057a3e3f32a8990a47d1b3f4c90ea79bff
babita <151219440+babita1234qwer@users.noreply.github.com>
2025-07-05 15:19:23 +0530

    Update leaderboard.js

--- src/routes/leaderboard.js
+++ src/routes/leaderboard.js
@@ -2,7 +2,7 @@ const express = require('express');
 const leaderrouter = express.Router();
 const { getLeaderboard } = require('../controllers/leaderboard');
 
-// GET /api/contest/:contestId/leaderboard
+
 leaderrouter.get('/contest/:contestId/leaderboard', getLeaderboard);
 
 module.exports = leaderrouter;
